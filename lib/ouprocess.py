Bugfix variance vs standard deviation

lib/ouprocess.py
@@ -8,12 +8,13 @@ def OUprocess(N, Nmax, mu, sf, tau):
 	exp = np.exp(-dt/tau)
 	norm = mu * (1-exp)
 	var = 0.5 * sf**2.0 * (1-np.exp(-2*dt/tau))
+	std = np.sqrt(var)
 
 	X = np.zeros(N)
-	X[0] = np.random.normal(mu,var)
+	X[0] = np.random.normal(mu,std)
 
 	for i in range(N-1):
 		E = exp * X[i] + norm
-		X[i+1] = np.random.normal(E,var)
+		X[i+1] = np.random.normal(E,std)
 
 	return (t, X)